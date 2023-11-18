d6fe3523afc45fda05cfb823819fdefddd464819
Gary Yu <hyu100@my.bcit.ca>
2023-11-18 13:39:02 -0800

    enable session information to persist in profile blocks for components

--- src/app/components/molecules/profileblock.tsx
+++ src/app/components/molecules/profileblock.tsx
@@ -1,11 +1,17 @@
 import BlockHeader2 from "../atoms/block/block-header-2";
 import GreetingHeaderSubtitle from "../atoms/greeting/greeting-header-subtitle";
+import {redirect} from "next/navigation"
 
-export default function ProfileBlock (props: {user: string, subtitle: string}) {
-    return (
-        <div>
-            <BlockHeader2 header={props.user} />
-            <GreetingHeaderSubtitle text={props.subtitle} />
-        </div>
-    )
+export default function ProfileBlock (props: {user: string | undefined | null, subtitle: string | undefined | null}) {
+
+    if(typeof props.user == "string" && typeof props.subtitle == "string") {
+        return (
+            <div>
+                <BlockHeader2 header={props.user} />
+                <GreetingHeaderSubtitle text={props.subtitle} />
+            </div>
+        )
+    } else {
+        redirect("/api/auth/signin?callbackUrl=/profile")
+    }
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,17 @@
-import type { NextAuthConfig } from "next-auth"
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthConfig } from "next-auth"
 import GitHub from "next-auth/providers/GitHub"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { db } from "@/db"
 
-export const authConfig = {
-  adapter: DrizzleAdapter(db),
+export const authConfig = { 
   providers: [GitHub],
+  adapter: DrizzleAdapter(db),
+  callbacks: {
+    async session({session, user}) {
+      session.user.id = user.id
+      return session
+    },
+  }
 } satisfies NextAuthConfig
 
 export const { 
