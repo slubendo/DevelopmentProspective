a47e4f20fcdce74356f8d201e9011de21d8cd925
Brett Gill <hello@brettgill.io>
2023-11-22 16:22:17 -0800

    home page styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import BlockHeader2 from './components/atoms/block/block-header-2';
 import GreetingCard from './components/organisms/greetingcard';
 import scholarshipData from '@/db/fake-schol.json';
 import ButtonActionText from './components/atoms/button/button-action-text';
+import AddCategoryIcon from './components/atoms/add-category';
 
 import { auth } from '@/auth';
 import { db } from '@/db';
-import { scholarships } from '@/db/schema/scholarships';
+
 
 export default async function Home() {
-  
+
   const session = await auth();
 
   // Process the data as per requirements
@@ -27,16 +28,50 @@ export default async function Home() {
     };
   });
 
+  // Change listTest2 to contain data with specs about athletics and ethnicity
+
+  const listTest2 = scholarshipData.map(item => {
+    return {
+      id: 2,
+      title: item.title,
+      content: item.content,
+      award: item.award,
+      deadline: item.deadline,
+      href: item.details,
+      src: item.src,
+      alt: item.alt
+    };
+  });
+
   return (
     <div>
-        <GreetingCard />
+      <GreetingCard />
 
-        {/* Scholarship header */}
-        <div className="flex justify-center mx-auto w-full pt-4 bg-slate-100 rounded-t-full">
-          <BlockHeader2 header="Scholarships For You" />
+      {/* Scholarship header */}
+      <div className="flex justify-center mx-auto w-full pt-4 bg-slate-100 rounded-t-full">
+        <BlockHeader2 header="Scholarships For You" />
+      </div>
+
+      <ScrollScholarshipCardSimple list={listTest} />
+
+      {/* Explore scholarship header */}
+      <div className="flex justify-center mx-auto w-full bg-slate-100">
+        <BlockHeader2 header="Explore" />
+      </div>
+      <div>
+        <div className="flex justify-center mx-auto pb-2 w-full bg-slate-100">
+          <ButtonActionText href="/" text="Athletics" />
+          <span className="px-2"></span>
+          <ButtonActionText href="/" text="Ethnicity" />
+          <span className="px-2"></span>
+          <button className="bg-blue-600 px-2 rounded-full">
+            <AddCategoryIcon />
+          </button>
         </div>
-
-        <ScrollScholarshipCardSimple list={listTest} />
+      </div>
+      <div className="bg-gray-100">
+        <ScrollScholarshipCardSimple list={listTest2} />
+      </div>
     </div>
   );
 }
