4f6d0e34aee2a51f6f5af4f9119fe67ba01a1575
Patrickhuiwork <patrick.hui.work@gmail.com>
2023-11-30 22:16:50 -0800

    profile page
    aligned the content to the right and changed the font sizing on components, styled the tab button to match figma

--- src/app/components/atoms/tab/tab-button.tsx
+++ src/app/components/atoms/tab/tab-button.tsx
@@ -2,10 +2,10 @@ export default function TabButton(props: { tab: string; focus: boolean }) {
     return (
       <div className="text-2xl">
         <p
-          className={`inline-block px-5 pb-1 ${
+          className={`inline-block px-5 pb-0.1 transition-all duration-300 ${
             props.focus
               ? 'text-azure-blue border-b-2 border-azure-blue transform scale-105'
-              : 'text-soft-black opacity-75 hover:text-azure-blue hover:border-azure-blue transition-all duration-300 transform hover:scale-105'
+              : 'text-soft-black opacity-75 hover:text-azure-blue hover:border-azure-blue transform hover:scale-105'
           }`}
         >
           {props.tab}
--- src/app/components/molecules/profileblock.tsx
+++ src/app/components/molecules/profileblock.tsx
@@ -1,21 +1,22 @@
-import BlockHeader2 from "../atoms/block/block-header-2";
 import GreetingHeaderSubtitle from "../atoms/greeting/greeting-header-subtitle";
-import {redirect} from "next/navigation"
+import { redirect } from "next/navigation";
 import Link from "next/link";
 
-export default function ProfileBlock (props: {user: string | undefined | null, subtitle: string | undefined | null}) {
-
-    if(typeof props.user == "string" && typeof props.subtitle == "string") {
-        return (
-            <div>
-                <BlockHeader2 header={props.user} />
-                <GreetingHeaderSubtitle text={props.subtitle} />
-                <Link href="/api/auth/signout?callbackUrl=/">
-                    <GreetingHeaderSubtitle text="Sign Out" />
-                </Link>
-            </div>
-        )
-    } else {
-        redirect("/api/auth/signin?callbackUrl=/profile")
-    }
+export default function ProfileBlock(props: {
+  user: string | undefined | null;
+  subtitle: string | undefined | null;
+}) {
+  if (typeof props.user === "string" && typeof props.subtitle === "string") {
+    return (
+      <div>
+        <p className="text-2xl font-medium">{props.user}</p>
+        <GreetingHeaderSubtitle text={props.subtitle} />
+        <Link href="/api/auth/signout?callbackUrl=/">
+          <GreetingHeaderSubtitle text="Sign Out" />
+        </Link>
+      </div>
+    );
+  } else {
+    redirect("/api/auth/signin?callbackUrl=/profile");
+  }
 }
