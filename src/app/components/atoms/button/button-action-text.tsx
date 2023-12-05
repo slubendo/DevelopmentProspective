51c270f753c8f7bf8b7ea7320565434ec568b76f
Patrickhuiwork <patrick.hui.work@gmail.com>
2023-12-05 15:38:03 -0800

    Update button-action-text.tsx

--- src/app/components/atoms/button/button-action-text.tsx
+++ src/app/components/atoms/button/button-action-text.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ButtonActionText(props: { href: string, text: string }) {
   return (
-    <Link href={props.href}>
+    <Link href={`$props.href`}>
       <div className="text-center p-2 bg-azure-blue text-white text-xs font-medium py-2 px-6 rounded-full transition-transform ease-in-out duration-300 transform hover:scale-105">
         {props.text}
       </div>
