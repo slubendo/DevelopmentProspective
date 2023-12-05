7b26c0dd2aec4c419fc19500ce4060e6a78f4814
Gary Yu <hyu100@my.bcit.ca>
2023-12-05 13:54:31 -0800

    fix issue with undefined in Link

--- src/app/components/atoms/button/button-action-text.tsx
+++ src/app/components/atoms/button/button-action-text.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function ButtonActionText(props: { href: string, text: string }) {
 
   return (
-    <Link href={props.href}>
+    <Link href={`${props.href}`}>
       <div className="text-center p-2 bg-blue-600 hover:bg-blue-900 text-white text-xs font-extralight py-2 px-4 rounded-full transform hover:-translate-y-1 w-18">
         {props.text}
       </div>
