213606f0f022e1ea96bdc926e3bdb169f9eda4e6
Brett Gill <hello@brettgill.io>
2023-12-07 23:07:36 -0800

    fix spacing between scholarships on profile

--- src/app/components/atoms/tab/tab-container.tsx
+++ src/app/components/atoms/tab/tab-container.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function TabContainer(props: { children: ReactNode }) {
   return (
     <div className="rounded-t-xl">
-      <div className="bg-gray-100 pt-2 mt-2 rounded-t-[80px]">
+      <div className="bg-gray-100 pt-2 my-4 rounded-t-[80px]">
         {props.children}
       </div>
     </div>
