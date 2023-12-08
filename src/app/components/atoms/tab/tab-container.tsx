8e3a417bf1697c636cdf0ad92848482a95116f5d
Patrickhuiwork <patrick.hui.work@gmail.com>
2023-12-07 21:14:51 -0800

    profile and scholarship page

--- src/app/components/atoms/tab/tab-container.tsx
+++ src/app/components/atoms/tab/tab-container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function TabContainer(props: {children:ReactNode}) {
     return (
-        <div className="bg-gray-100 rounded-lg">
+        <div className="bg-gray-100 rounded-xl">
             {props.children}
         </div>
     )
